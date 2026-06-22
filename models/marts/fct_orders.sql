ee48c16f122a654bbf6675044b1f580be6d474e5
YoudeepSK <youdeep111@gmail.com>
2026-06-22 11:52:20 +0530

    done with dbt fundamentals

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,30 +1,49 @@
-with orders as  (
-    select * from {{ ref ('stg_jaffle_shop_orders' )}}
+{{ config(
+    materialized='incremental',
+    incremental_stragegy='merge',
+    unique_key='order_id'
+) }}
+
+with orders as (
+    select *
+    from {{ ref('stg_jaffle_shop_orders') }}
 ),
 
 payments as (
-    select * from {{ ref ('stg_stripe_payments') }}
+    select *
+    from {{ ref('stg_stripe_payments') }}
 ),
 
 order_payments as (
     select
         order_id,
-        sum (case when status = 'success' then amount end) as amount
-
+        sum(case when status = 'success'
+                 then amount end) as amount
     from payments
     group by 1
 ),
 
- final as (
+final as (
 
     select
         orders.order_id,
         orders.customer_id,
         orders.order_date,
-        coalesce (order_payments.amount, 0) as amount
-
+        coalesce(order_payments.amount, 0) as amount
     from orders
     left join order_payments using (order_id)
+
 )
 
-select * from final
+select *
+from final f
+
+{% if is_incremental() %}
+where f.order_date >
+(
+    select max(order_date)
+    from {{ this }}
+)
+{% endif %}
+
+order by order_date desc
